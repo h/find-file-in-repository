ff3e352038fa3c18d4e2e15ca9a5f47364e58252
Samuel Hoffstaetter <samuel@hoffstaetter.com>
2012-11-13 02:53:23 -0500

    minor edit to make README and .el descriptions match

--- find-file-in-repository.el
+++ find-file-in-repository.el
@@ -29,11 +29,10 @@
 ;; using find-file, (or ido-find-file), which makes it a suitable
 ;; replacement for the "C-x f" keybinding.
 ;;
-;; It is similar to, but much faster and more robust than the
-;; find-file-in-project package. It relies on git/mercurial/etc to
-;; provide fast cached file name completions, which means that
-;; repository features such as .gitignore/.hgignore/etc are fully
-;; supported out of the box.
+;; It is similar to, but faster and more robust than the find-file-in-project
+;; package. It relies on git/mercurial/etc to provide fast cached file name
+;; completions, which means that repository features such as
+;; .gitignore/.hgignore/etc are fully supported out of the box.
 ;;
 ;; This library currently has support for:
 ;;     git, mercurial, darcs, bazaar, monotone, svn
